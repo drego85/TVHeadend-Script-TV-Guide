2242071123ba1977104fb58f1666d2f0b2715cb3
Andre Draghetti <drego85@draghetti.it>
2018-09-17 13:31:17 +0200

    Script autoinstall - First Commit

---
+++ autoinstall.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install python3 python3-pip
+sudo cp tv_grab_italy* /usr/bin/
+sudo mv /usr/bin/tv_grab_italy_basic_url.py /usr/bin/tv_grab_italy_basic_url
+sudo mv /usr/bin/tv_grab_italy_sky_url.py /usr/bin/tv_grab_italy_sky_url
+sudo mv /usr/bin/tv_grab_italy_sport_movies_url.py /usr/bin/tv_grab_italy_sport_movies_url
+sudo chmod 777 /usr/bin/tv_grab_italy*
+sudo /etc/init.d/tvheadend restart
